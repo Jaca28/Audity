311111ae5eac12d0167d55515fe6ff2a41f210f4
Junior Calle <jrcalleac@gmail.com>
2023-10-21 21:35:55 -0500

    Plus: requirements view added.

---
+++ Dapp/routes/requirements.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET requirement page. */
+router.get('/', function (req, res, next) {
+    let sidebar = false
+    let roll
+    if (req.session.user) {
+        sidebar = true
+        roll = req.session.roll
+    }
+    res.render('requirements', { title: 'Block E Design', sidebar: sidebar, roll: roll });
+});
+
+module.exports = router;
