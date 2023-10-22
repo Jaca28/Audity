174acef5ab73bd18be5db77f2e8de75d14028496
Junior Calle <jrcalleac@gmail.com>
2023-10-21 21:34:46 -0500

    Plus: registerRequest view added.

---
+++ Dapp/routes/registerRequest.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', function(req, res, next) {
+    let sidebar = false
+    let roll
+    if (req.session.user) {
+        sidebar = true
+        roll = req.session.roll
+    }
+    console.log(roll)
+    res.render('registerRequest', { title: 'Block E Design', sidebar: sidebar, roll: roll });
+
+});
+
+module.exports = router;
